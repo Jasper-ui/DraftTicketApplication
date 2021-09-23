cd8a8c7db9556043426ecfd567dd4b56be897950
Jasper <k.tuna4@gmail.com>
2021-09-23 20:25:19 +0300

    Added logger

--- src/main/java/com/product/ticketapp/controllers/DraftTicketController.java
+++ src/main/java/com/product/ticketapp/controllers/DraftTicketController.java
@@ -3,6 +3,7 @@ package com.product.ticketapp.controllers;
 import com.product.ticketapp.controllers.requests.DraftTicketRequest;
 import com.product.ticketapp.controllers.responses.DraftTicketResponse;
 import com.product.ticketapp.interfaces.TicketService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Slf4j
 @RestController
 public class DraftTicketController {
   private final TicketService ticketService;
@@ -23,6 +25,7 @@ public class DraftTicketController {
   @PostMapping("/")
   @ResponseBody
   public DraftTicketResponse generateTickets(@Valid @RequestBody DraftTicketRequest draftTicket) {
+    log.debug("Draft ticket with id received. Calling Draft ticket service");
     return ticketService.processDraftTicket(draftTicket);
   }
 }
--- src/main/java/com/product/ticketapp/services/DraftTicketServiceImpl.java
+++ src/main/java/com/product/ticketapp/services/DraftTicketServiceImpl.java
@@ -5,6 +5,7 @@ import com.product.ticketapp.controllers.responses.DraftTicketResponse;
 import com.product.ticketapp.entities.Passenger;
 import com.product.ticketapp.entities.Ticket;
 import com.product.ticketapp.interfaces.TicketService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
@@ -12,6 +13,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 public class DraftTicketServiceImpl implements TicketService {
 
@@ -25,10 +27,16 @@ public class DraftTicketServiceImpl implements TicketService {
     BigDecimal travelRate = getTravelRate(draftTicket.getArrival(), draftTicket.getDeparture());
     BigDecimal taxRate = getTaxRate();
 
+    log.debug("Today's travel rate is {} and tax rate is {}", travelRate, taxRate);
+
     List<Ticket> individualTickets = new ArrayList<>();
 
     for (Passenger passenger : draftTicket.getPassengerList()) {
       individualTickets.add(createIndividualTicket(passenger, travelRate, taxRate));
+    }
+
+    if (individualTickets.isEmpty()) {
+      log.error("Created individual tickets list is empty");
     }
 
     return createResponseTicket(draftTicket, individualTickets);
@@ -46,6 +54,12 @@ public class DraftTicketServiceImpl implements TicketService {
 
   private Ticket createIndividualTicket(
       Passenger passenger, BigDecimal travelRate, BigDecimal taxRate) {
+
+    log.debug(
+        "Creating individual ticket for passenger {} {}",
+        passenger.getFirstName(),
+        passenger.getLastName());
+
     String firstName = passenger.getFirstName();
     String lastName = passenger.getLastName();
 
@@ -67,6 +81,8 @@ public class DraftTicketServiceImpl implements TicketService {
 
   private DraftTicketResponse createResponseTicket(
       DraftTicketRequest draftTicket, List<Ticket> individualTickets) {
+    log.debug("Creating response ticket for draft ticket with ID");
+
     DraftTicketResponse tickets = new DraftTicketResponse();
 
     tickets.setDeparture(draftTicket.getDeparture());
